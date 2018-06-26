16d76c959353ac4a29a6e4b936299a8e812a3b53
himanshu4444 <nagpalh@hcl.com>
2018-06-26 21:05:58 +0530

    implement factory method

--- microservice-ecms-notification/src/main/java/com/ecms/notification/strategyImpl/MailStrategy.java
+++ microservice-ecms-notification/src/main/java/com/ecms/notification/strategyImpl/MailStrategy.java
@@ -1,17 +1,35 @@
 package com.ecms.notification.strategyImpl;
 
+import javax.mail.MessagingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.ecms.model.Event;
 import com.ecms.notification.strategy.NotificationStrategy;
+import com.ecms.service.NotificationService;
+import com.ecms.serviceImpl.NotificationServiceImpl;
 
 public class MailStrategy implements NotificationStrategy
 {
-    
+    @Autowired
+    NotificationService notificationService; 
 
+    private static Logger log = LoggerFactory.getLogger(MailStrategy.class);
+    
     @Override
-    public String sendMessage(Event event)
+    public String sendMessage(Event event) 
     {
-        
-        return null;
+        try
+        {
+            notificationService.sendMail(event);
+        }
+        catch (MessagingException e)
+        {
+            log.info(e.getMessage());
+        }
+        return "success";
     }
 
 }
--- microservice-ecms-notification/src/main/java/com/ecms/utils/UserUtility.java
+++ microservice-ecms-notification/src/main/java/com/ecms/utils/UserUtility.java
@@ -1,11 +1,12 @@
 package com.ecms.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.ecms.dao.UserDao;
 import com.ecms.model.Event;
 import com.ecms.model.User;
-
+@Component
 public class UserUtility
 {
     @Autowired
